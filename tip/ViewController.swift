1d8c73476fea647fef94699933cab50f049a46e7
Rebecca <rebecca_517@hotmail.com>
2020-08-24 13:51:09 -0400

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Rebecca Shi on 8/24/20.
+//  Copyright © 2020 Rebecca Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
